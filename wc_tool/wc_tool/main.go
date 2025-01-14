25b2622228b4b1c08399b2f0a3a37b06dcaf43b0
daiki-kim <kimdaiki5021@gmail.com>
2025-01-15 06:55:41 +0900

    add a feature to print line count of file contents

--- wc_tool/wc_tool/main.go
+++ wc_tool/wc_tool/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -9,8 +10,9 @@ import (
 )
 
 func main() {
-	var flagByte bool
-	flag.BoolVar(&flagByte, "c", false, "print byte of file contents")
+	var flagByte, flagLine bool
+	flag.BoolVar(&flagByte, "c", false, "print byte count of file contents")
+	flag.BoolVar(&flagLine, "l", false, "print line count of file contents")
 	flag.Parse()
 
 	fName := flag.Arg(0)
@@ -27,6 +29,26 @@ func main() {
 		if err != nil {
 			log.Fatalln("failed to read file:", err)
 		}
+		file.Seek(0, 0)
 		fmt.Println(len(fBytes))
+	}
+
+	if flagLine {
+		buf := make([]byte, 32*1024)
+		lCnt := 0
+		lSep := []byte{'\n'}
+		for {
+			c, err := file.Read(buf)
+			lCnt += bytes.Count(buf[:c], lSep)
+
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln(err)
+			}
+		}
+		file.Seek(0, 0)
+		fmt.Println(lCnt)
 	}
 }
